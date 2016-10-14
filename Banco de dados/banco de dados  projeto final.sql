2860c8fc08a25ae8915aeaaed3699d05ea283354
alexsaguiar <alexsaguiar@yahoo.com.br>
2016-10-14 19:05:40 -0300

    banco de dados
    correção do banco de dados

--- Banco de dados/banco de dados  projeto final.sql
+++ Banco de dados/banco de dados  projeto final.sql
@@ -10,3 +10,8 @@ create table cidades (codigo_cidade integer primary key, nome_cidade varchar(100
 alter table clientes add constraint fk_cidades foreign key (codigo_cidade) references  cidades(codigo_cidade);
 
 create table historicos (codigo_historico integer primary key, pendente varchar(500), tb_concluido varchar(500))
+
+
+alter table clientes add column codigo_historico integer
+
+alter table clientes add constraint pk_historicos foreign key (codigo_historico) references historicos(codigo_historico)
